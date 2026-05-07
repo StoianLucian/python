4d33c5f30d3a7550bcda3173ccc625dc29256f85
Lucian Stoian <lucians@192.168.1.135>
2026-05-07 15:04:02 +0300

    add current date to file

--- repositories/files_repository.py
+++ repositories/files_repository.py
@@ -1,6 +1,7 @@
 import string
 from sqlalchemy.orm import Session
 from db.schemas.file import File
+from datetime import datetime
 
 
 def upload_file_db(filename: string, storageKey: string, size: int, type: string, createdBy: string, db: Session):
@@ -11,6 +12,7 @@ def upload_file_db(filename: string, storageKey: string, size: int, type: string
         file_size=size,
         file_type=type,
         created_by=createdBy,
+        created_at=datetime.now()
     )
 
     db.add(file)
