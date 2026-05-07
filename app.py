f0a6681b9de2c38638e24c6ce5f8c0ea303ec6e3
Lucian Stoian <lucians@192.168.1.135>
2026-05-07 11:33:01 +0300

    add origin

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",  # same dev server, different host string in the browser
+    "https://python-react-ehkr-git-main-stoianlucians-projects.vercel.app"
 ]
 
 app.add_middleware(
