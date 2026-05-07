cae22cabe59d3bcd04d9f25a76f83f0bb76a8497
Lucian Stoian <lucians@192.168.1.135>
2026-05-07 13:42:07 +0300

    modify cors2

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://python-react-ehkr-b0lzjbrwf-stoianlucians-projects.vercel.app"
+        "https://python-react-ehkr-git-main-stoianlucians-projects.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
