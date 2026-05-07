2c7ec1c93b34f4c26419b96dea29c735a346f3a1
Lucian Stoian <lucians@192.168.1.135>
2026-05-07 14:38:56 +0300

    log token

--- repositories/auth_repository.py
+++ repositories/auth_repository.py
@@ -73,6 +73,9 @@ def login_user_db(loginData: LoginRequest, response: Response, db: Session):
 
 def check_token(request: Request):
     token = request.cookies.get(TOKEN_NAME)
+    
+    
+    print(token, "token")
     if token is None:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
